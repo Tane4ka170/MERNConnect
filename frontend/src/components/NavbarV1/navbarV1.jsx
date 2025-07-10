e331537456c53826fb76f60f331797b94e42af85
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-10 16:00:44 +0300

    Feat: implemented NavbarV1 layout

--- frontend/src/components/NavbarV1/navbarV1.jsx
+++ frontend/src/components/NavbarV1/navbarV1.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const navbarV1 = () => {
-  return <div>navbarV1</div>;
+const NavbarV1 = () => {
+  return (
+    <nav className="w-[100%] bg-gray-500 md:px-[100px] px-[20px] flex justify-between py-4 box-border">
+      <div className="flex justify-between">
+        <div className="flex gap-0 items-center cursor-pointer">
+          <h3 className="text-blue-900 font-bold text-3xl">NetWorkin</h3>
+          <img
+            src={"https://cdn-icons-png.flaticon.com/512/942/942749.png"}
+            alt="logo"
+            className="w-5 h-5"
+          />
+        </div>
+      </div>
+    </nav>
+  );
 };
 
-export default navbarV1;
+export default NavbarV1;
