d2c9bca87130579a4bcc91e3b84ce440296e5490
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-10 16:22:57 +0300

    Feat: implemented LandingPage layout

---
+++ frontend/src/pages/LandingPage/landingPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div className="my-4 py-[50px] md:pl-[120px] px-5 md:flex justify-between">
+      <div className="md:w-[40%]">
+        <div className="text-4xl mx-auto text-gray-300">
+          Step Into Your Professional Network
+          <div className="my-3 flex mx-auto mt-[20px] bg-white gap-2 rounded-3xl w-[70%] text-black cursor-pointer">
+            Google button
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
