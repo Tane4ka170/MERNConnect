70e6d2be9c1329f7e0d44aaeee707091a411a8a4
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-10 16:21:53 +0300

    Feat: integrate LandingPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import NavbarV1 from "./components/NavbarV1/navbarV1";
+import LandingPage from "./pages/LandingPage/landingPage";
 
 function App() {
   return (
     <>
       <div className="bg-gray-600 w-[100%] h-[100%] box-border">
         <NavbarV1 />
+        <LandingPage />
       </div>
     </>
   );
