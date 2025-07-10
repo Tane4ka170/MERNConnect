71da74acf892cecb1a7a74de76f0c0a47917e4c0
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-10 17:15:55 +0300

    Feat: expanded LandingPage content

--- frontend/src/pages/LandingPage/landingPage.jsx
+++ frontend/src/pages/LandingPage/landingPage.jsx
@@ -9,7 +9,39 @@ const LandingPage = () => {
           <div className="my-3 flex mx-auto mt-[20px] bg-white gap-2 rounded-3xl w-[70%] text-black cursor-pointer">
             Google button
           </div>
+          <div className="flex mx-auto mt-[20px] py-2 px-2 bg-white gap-2 rounded-3xl items-center w-[70%]  justify-between text-black hover:bg-gray-600 border-2 cursor-pointer">
+            Log in using your email
+          </div>
+          <div className="mx-auto mb-4 text-sm w-[70%] mt-6">
+            By selecting “Continue,” you confirm that you agree to{" "}
+            <span className="text-blue-100 cursor-pointer hover:underline">
+              NetWorkin’s User Agreement
+            </span>
+            ,{" "}
+            <span className="text-blue-100 cursor-pointer hover:underline">
+              Privacy Policy
+            </span>{" "}
+            , and{" "}
+            <span className="text-blue-100 cursor-pointer hover:underline">
+              Cookie Policy
+            </span>
+            .
+          </div>
+          <div
+            href="/signup"
+            className="mx-auto text-center mb-4 text-lg w-[70%] mt-4"
+          >
+            {" "}
+            Not a member yet?{" "}
+            <span className="text-blue-200 cursor-pointer hover:underline">
+              Sign up for NetWorkin
+            </span>
+          </div>
         </div>
+      </div>
+
+      <div className="md:w-[50%] h-120">
+        <img src="" alt="Image" className="h-full w-full" />
       </div>
     </div>
   );
