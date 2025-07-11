244343f4ed7942e268fa176d3e310f06766f5d2d
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-11 14:48:58 +0300

    Feat: added Footer component

---
+++ frontend/src/components/Footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-[100%] bg-gray-950 flex justify-center">
+      <div className="md:p-3 w-[100%] flex flex-col items-center py-4"></div>
+    </div>
+  );
+};
+
+export default Footer;
