16be465f8fdbde0a32bea7d8f07a7a35aa00c405
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-12 15:24:54 +0300

    Feat: added routing & sign-up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,18 @@ import "./App.css";
 import Footer from "./components/Footer/footer";
 import NavbarV1 from "./components/NavbarV1/navbarV1";
 import LandingPage from "./pages/LandingPage/landingPage";
+import { Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp/signUp";
 
 function App() {
   return (
     <>
-      <div className="bg-gray-600 w-[100%] h-[100%] box-border">
+      <div className="bg-gray-50 w-[100%] h-[100%] box-border">
         <NavbarV1 />
-        <LandingPage />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signUp" element={<SignUp />} />
+        </Routes>
         <Footer />
       </div>
     </>
