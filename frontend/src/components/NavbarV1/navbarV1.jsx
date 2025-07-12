f7ac0aae3b1db556c3b6f721a500c0c0f925cf12
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-12 15:27:50 +0300

    Feat: nav links to Landing & SignUp

--- frontend/src/components/NavbarV1/navbarV1.jsx
+++ frontend/src/components/NavbarV1/navbarV1.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavbarV1 = () => {
   return (
     <nav className="w-[100%] bg-gray-500 md:px-[100px] px-[20px] flex justify-between py-4 box-border">
-      <div className="flex justify-between">
+      <Link to={"/"} className="flex justify-between">
         <div className="flex gap-0 items-center cursor-pointer">
           <h3 className="text-blue-900 font-bold text-3xl">NetWorkin</h3>
           <img
@@ -12,12 +13,15 @@ const NavbarV1 = () => {
             className="w-5 h-5"
           />
         </div>
-      </div>
+      </Link>
 
       <div className="flex box-border md:gap-4 gap-2 items-center justify-center">
-        <div className="md:px-4 md:py-2 box-border text-white rounded-3xl text-xl hover:bg-gray-700 cursor-pointer">
+        <Link
+          to={"/signUp"}
+          className="md:px-4 md:py-2 box-border text-white rounded-3xl text-xl hover:bg-gray-700 cursor-pointer"
+        >
           Sign Up Today
-        </div>
+        </Link>
         <div className="px-4 py-2 box-border border-1 text-blue-400 border-blue-400 rounded-3xl text-xl hover:bg-blue-200 cursor-pointer">
           Log In
         </div>
