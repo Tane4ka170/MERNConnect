b4f028aae4943bbaaaa49a70fe12b01cf335f3bb
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-14 14:29:20 +0300

    Feat: Integrated GoogleLoginComp in Login page

--- frontend/src/pages/Login/login.jsx
+++ frontend/src/pages/Login/login.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import GoogleLoginComp from "../../components/GoogleLogin/googleLoginComp";
 
 const Login = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center">
       <div className="w-[85%] md:w-[28%] shadow-xl rounded-sm box p-10">
         <div className="text-3xl">Sign In</div>
-        <div className="my-5">Goggle login</div>
+        <div className="my-5">
+          <GoogleLoginComp />
+        </div>
 
         <div className="flex items-center gap-2">
           <div className="border-b-2 border-gray-500 w-[45%]" />
