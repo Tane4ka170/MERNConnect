e254b9aaa0afbea7b45ff22bc1931c74b8b82893
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-20 19:00:55 +0300

    Feat: Added Navbar2 component

---
+++ frontend/src/components/Navbar2/navbar2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar2 = () => {
+  return (
+    <div className="bg-white h-13 flex justify-between py-1 px-5 xl:px-50 fixed top-0 w-[100%] z-1000">
+      navbar2
+    </div>
+  );
+};
+
+export default Navbar2;
