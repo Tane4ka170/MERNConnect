8436270b6c8193ed25f103757bb76c3b487e8dd7
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-25 15:27:57 +0300

    Feat: Implemented Navbar2 w/ search & dropdown

--- frontend/src/components/Navbar2/navbar2.jsx
+++ frontend/src/components/Navbar2/navbar2.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import { useState } from "react";
+import "./navbar2.css";
 
 const Navbar2 = () => {
+  const [dropDown, setDropDown] = useState(false);
+
   return (
     <div className="bg-white h-13 flex justify-between py-1 px-5 xl:px-50 fixed top-0 w-[100%] z-1000">
-      navbar2
+      <div className="flex gap-2 items-center">
+        <div>
+          <img
+            src={"https://cdn-icons-png.flaticon.com/512/942/942749.png"}
+            alt="logo"
+            className="w-8 h-8"
+          />
+        </div>
+
+        <div className="relative">
+          <input
+            type="text"
+            className="searchInput w-70 bg-gray-50"
+            placeholder="Search"
+          />
+
+          {dropDown && (
+            <div className="absolute w-88 left-0 bg-gray-400">
+              <div className="flex gap-2 items-center border-b-1 mb-1 cursor-pointer">
+                <div>
+                  <img
+                    src={
+                      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/User_icon-cp.png/1200px-User_icon-cp.png"
+                    }
+                    alt=""
+                    className="w-10 h-10 rounded-full"
+                  />
+                </div>
+
+                <div>Danish</div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
