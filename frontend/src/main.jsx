f03023063f73a7115169c876887c4c0412a91c0e
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-25 15:28:22 +0300

    Fix: Updated Google Auth Key env var

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_AUTH_KEY}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_KEY}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
