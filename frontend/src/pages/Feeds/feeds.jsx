64997b9277f7414e1780289023c981a272172541
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-26 16:29:07 +0300

    Feat: Initialized Feeds page layout w/ProfileCard

--- frontend/src/pages/Feeds/feeds.jsx
+++ frontend/src/pages/Feeds/feeds.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import ProfileCard from "../../components/ProfileCard/ProfileCard";
 
 const Feeds = () => {
-  return <div>F</div>;
+  return (
+    <div className="px-5 xl:px-50 py-9 flex gap-5 w-full mt-5 bg-gray-300">
+      {/* Left */}
+      <div className="w-[21%] sm:block sm:w-[23%] hidden py-5">
+        <div className="h-fit">
+          <ProfileCard />
+        </div>
+      </div>
+
+      {/* Middle */}
+      <div></div>
+
+      {/* Right */}
+      <div></div>
+    </div>
+  );
 };
 
 export default Feeds;
