87563914b3157460709c6f8bf6d5dd4af7f449e8
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-27 16:32:16 +0300

    Feat: Added conditional card padding

--- frontend/src/components/Card/card.jsx
+++ frontend/src/components/Card/card.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const Card = (props) => {
   return (
-    <div className="w-full h-[100%] flex flex-col border-1 border-gray-100 bg-white rounded-md">
+    <div
+      className={`w-full h-[100%] flex flex-col border-1 border-gray-100 bg-white rounded-md ${
+        props.padding ? "p-5" : "p-0"
+      }`}
+    >
       {props.children}
     </div>
   );
