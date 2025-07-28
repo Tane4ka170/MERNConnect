b2e742e1981aac0873ad0d27ae470f1dc72dd442
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-28 17:51:49 +0300

    Feat: Added post interaction & comment section

--- frontend/src/components/Post/post.jsx
+++ frontend/src/components/Post/post.jsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
 import Card from "../Card/card";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
+import CommentIcon from "@mui/icons-material/Comment";
+import SendIcon from "@mui/icons-material/Send";
 
 const Post = () => {
   const [seeMore, setSeeMore] = useState(false);
+  const [comment, setComment] = useState(false);
 
   const desc = `Over the last few months, our team at Issac and Sons has been working hard to modernize our internal development workflows. We recently rolled out a fully containerized CI/CD pipeline using GitHub Actions and Docker, and the impact on our deployment time has been incredible — 60% faster on average. We’re now focusing on performance monitoring and integrating more automated testing across all services. Huge thanks to the dev and infra teams for pushing these initiatives forward. This is how real change happens — one step at a time. #Engineering #Leadership #DevOps #ContinuousDelivery`;
+
+  const handleSendComment = (e) => {
+    e.preventDefault();
+  };
   return (
     <Card padding={0}>
       <div className="flex gap-3 p-4">
@@ -44,7 +53,88 @@ const Post = () => {
         />
       </div>
 
-      <div className="my-2 p-4 flex justify-between items-center"></div>
+      <div className="my-2 p-4 flex justify-between items-center">
+        <div className="flex gap-1 items-center">
+          <ThumbUpIcon sx={{ color: "blue", fontSize: 20 }} />{" "}
+          <div className="text-sm text-gray-900"> 92 Likes</div>
+        </div>
+        <div className="flex gap-1 items-center">
+          <ThumbUpIcon sx={{ color: "blue", fontSize: 20 }} />{" "}
+          <div className="text-sm text-gray-900"> 6 Comments</div>
+        </div>
+      </div>
+
+      <div className="flex p-1">
+        <div className="w-[33%] justify-center flex gap-2 items-center border-r-1 border-gray-100 p-2 cursor-pointer hover:bg-gray-400">
+          <ThumbUpIcon sx={{ fontSize: 22, color: "blue" }} /> <span>Like</span>{" "}
+        </div>
+        <div
+          className="w-[33%] justify-center flex gap-2 items-center border-r-1 border-gray-100 p-2 cursor-pointer hover:bg-gray-400"
+          onClick={() => setComment(true)}
+        >
+          <CommentIcon sx={{ fontSize: 22, color: "blue" }} />{" "}
+          <span>Comment</span>{" "}
+        </div>
+        <div className="w-[33%] justify-center flex gap-2 items-center border-r-1 border-gray-100 p-2 cursor-pointer hover:bg-gray-400">
+          <SendIcon sx={{ fontSize: 22, color: "blue" }} /> <span>Share</span>{" "}
+        </div>
+      </div>
+
+      {/* Comment section */}
+      {comment && (
+        <div className="p-4 w-full">
+          <div className="flex gap-2 items-center">
+            <img
+              src={
+                "https://static.vecteezy.com/system/resources/previews/019/879/186/non_2x/user-icon-on-transparent-background-free-png.png"
+              }
+              alt="User"
+              className="rounded-full w-12 h-12 border-2 border-white cursor-pointer"
+            />
+
+            <form className="w-full flex gap-2" onSubmit={handleSendComment}>
+              <input
+                type="text"
+                placeholder="Write a comment..."
+                className="w-full border-1 py-3 px-5 rounded-3xl hover:bg-gray-500"
+              />
+              <button
+                type="submit"
+                className="cursor-pointer bg-blue-400 text-white rounded-3xl py-1 px-3"
+              >
+                Send
+              </button>
+            </form>
+          </div>
+
+          {/* Other's comment section */}
+          <div className="w-full p-4">
+            <div className="my-4">
+              <div className="flex gap-3">
+                <img
+                  src={
+                    "https://static.vecteezy.com/system/resources/previews/019/879/186/non_2x/user-icon-on-transparent-background-free-png.png"
+                  }
+                  alt="User"
+                  className="rounded-full w-12 h-12 border-2 border-white cursor-pointer"
+                />
+                <div className="cursor-pointer">
+                  <div className="text-md">Jarwadi Siregar</div>
+                  <div className="text-sm text-gray-950">
+                    Engineering Manager
+                  </div>
+                </div>
+              </div>
+
+              <div className="px-11 my-2">
+                This is impressive, Jarwadi! 🚀 Thanks for sharing the results —
+                we’ve been thinking about containerizing our pipeline too, and
+                this just gave me the push I needed.
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </Card>
   );
 };
