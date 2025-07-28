fe2e792d477db21a0f9fe47bf052722ed6a6739e
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-28 16:53:33 +0300

    Feat: Added toggleable post content & media

--- frontend/src/components/Post/post.jsx
+++ frontend/src/components/Post/post.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import { useState } from "react";
 import Card from "../Card/card";
 
 const Post = () => {
+  const [seeMore, setSeeMore] = useState(false);
+
+  const desc = `Over the last few months, our team at Issac and Sons has been working hard to modernize our internal development workflows. We recently rolled out a fully containerized CI/CD pipeline using GitHub Actions and Docker, and the impact on our deployment time has been incredible — 60% faster on average. We’re now focusing on performance monitoring and integrating more automated testing across all services. Huge thanks to the dev and infra teams for pushing these initiatives forward. This is how real change happens — one step at a time. #Engineering #Leadership #DevOps #ContinuousDelivery`;
   return (
     <Card padding={0}>
       <div className="flex gap-3 p-4">
@@ -21,7 +24,27 @@ const Post = () => {
         </div>
       </div>
 
-      <div className="text-md p-4"></div>
+      <div className="text-md p-4 my-3 whitespace-pre-line flex-grow">
+        {seeMore ? desc : `${desc.slice(0, 100)}...`}{" "}
+        <span
+          className="cursor-pointer text-gray-700"
+          onClick={() => setSeeMore((prev) => !prev)}
+        >
+          {seeMore ? "Show Less" : "Show More"}
+        </span>
+      </div>
+
+      <div className="w-[100%] h-[300px]">
+        <img
+          src={
+            "https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg?cs=srgb&dl=pexels-bri-schneiter-28802-346529.jpg&fm=jpg"
+          }
+          alt=""
+          className="w-full h-full"
+        />
+      </div>
+
+      <div className="my-2 p-4 flex justify-between items-center"></div>
     </Card>
   );
 };
