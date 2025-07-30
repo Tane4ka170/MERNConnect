8cb75e954c66b7ca539728aad74a7bc752b36882
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-30 15:46:43 +0300

    Feat: Added basic AddModal component

---
+++ frontend/src/components/AddModal/addModal.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const AddModal = () => {
+  return (
+    <div className="">
+      <div className="flex gap-4 items-center">
+        <div className="relative">
+          <img
+            src={
+              "https://static.vecteezy.com/system/resources/previews/019/879/186/non_2x/user-icon-on-transparent-background-free-png.png"
+            }
+            alt="User"
+            className="w-15 h-15 rounded-full"
+          />
+        </div>
+        <div className="text-2xl">Qadim Virk</div>
+      </div>
+    </div>
+  );
+};
+
+export default AddModal;
