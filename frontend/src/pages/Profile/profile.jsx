689716b79aff03094069214082d9cf178ea944f1
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-08-03 16:14:48 +0300

    Feat: Implemented full profile page layout

--- frontend/src/pages/Profile/profile.jsx
+++ frontend/src/pages/Profile/profile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import EditIcon from "@mui/icons-material/Edit";
 import Advertisement from "../../components/Advertisement/advertisement";
 import Card from "../../components/Card/card";
 
@@ -12,6 +12,9 @@ const Profile = () => {
             <Card padding={0}>
               <div className="w-full h-fit">
                 <div className="relative w-full h-[200px]">
+                  <div className="absolute cursor-pointer top-3 right-3 z-20 w-[35px] flex justify-center items-center h-[35px] rounded-full p-3 bg-white">
+                    <EditIcon />
+                  </div>
                   <img
                     src={
                       "https://sb.kaleidousercontent.com/67418/1920x1100/ffa7b88ebb/transparent.png"
@@ -31,24 +34,104 @@ const Profile = () => {
                 </div>
 
                 <div className="mt-10 relative px-8 py-2">
+                  <div className="absolute cursor-pointer top-3 right-3 z-20 w-[35px] flex justify-center items-center h-[35px] rounded-full p-3 bg-white">
+                    <EditIcon />
+                  </div>
                   <div className="w-full">
                     <div className="text-2xl">Qadim Virk</div>
                     <div className="text-gray-950">
-                      I am a Full-Stack Developer{" "}
+                      I work as a Full-Stack Developer{" "}
                     </div>
                     <div className="text-sm text-gray-400">
                       Hinjawadi, India
                     </div>
-                    <div className="text-md text-blue-600 w-fit cursor-pointer">
+                    <div className="text-md text-blue-600 w-fit cursor-pointer hover:underline">
                       81 connections
+                    </div>
+
+                    <div className="md:flex w-full justify-between">
+                      <div className="my-5 flex gap-5">
+                        <div className="cursor-pointer p-2 border-1 rounded-lg bg-blue-600 text-white font-semibold">
+                          Available for
+                        </div>
+                        <div className="cursor-pointer p-2 border-1 rounded-lg bg-blue-600 text-white font-semibold">
+                          Send
+                        </div>
+                        <div className="cursor-pointer p-2 border-1 rounded-lg bg-blue-600 text-white font-semibold">
+                          Sign out
+                        </div>
+                      </div>
+                      <div className="my-5 flex gap-5">
+                        <div className="cursor-pointer p-2 border-1 rounded-lg bg-blue-600 text-white font-semibold">
+                          Send a note
+                        </div>
+                        <div className="cursor-pointer p-2 border-1 rounded-lg bg-blue-600 text-white font-semibold">
+                          Add connection
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </Card>
           </div>
+
+          <div className="mt-5">
+            <Card padding={1}>
+              <div className="flex justify-between items-center">
+                <div className="text-xl">About</div>
+                <div className="cursor-pointer">
+                  <EditIcon />
+                </div>
+              </div>
+              <div className="text-gray-800 text-md w-[80%]">
+                Passionate and results-driven Full-Stack Developer with over 5
+                years of experience building scalable web applications and
+                modern digital solutions. Skilled in JavaScript, React, Node.js,
+                and cloud technologies. At Issac and Sons, I focus on delivering
+                end-to-end software solutions that enhance performance, user
+                experience, and business growth. I'm always excited to
+                collaborate on innovative projects, solve complex problems, and
+                continuously improve my skills. Based in Hinjawadi — the heart
+                of India’s tech scene — I thrive in fast-paced environments
+                where creativity and code meet.
+              </div>
+            </Card>
+          </div>
+
+          <div className="mt-5">
+            <Card padding={1}>
+              <div className="flex justify-between items-center">
+                <div className="text-xl">About</div>
+              </div>
+              <div className="text-gray-800 text-md my-2 w-full flex gap-4 flex-wrap">
+                <div className="py-2 px-3 cursor-pointer bg-blue-100 rounded-lg">
+                  Redux / Zustand / Context API
+                </div>
+                <div className="py-2 px-3 cursor-pointer bg-blue-100 rounded-lg">
+                  Postman
+                </div>
+                <div className="py-2 px-3 cursor-pointer bg-blue-100 rounded-lg">
+                  REST APIs & GraphQL
+                </div>
+                <div className="py-2 px-3 cursor-pointer bg-blue-100 rounded-lg">
+                  MongoDB / Mongoose
+                </div>
+                <div className="py-2 px-3 cursor-pointer bg-blue-100 rounded-lg">
+                  AWS / Azure / Vercel / Netlify
+                </div>
+                <div className="py-2 px-3 cursor-pointer bg-blue-100 rounded-lg">
+                  TypeScript
+                </div>
+                <div className="py-2 px-3 cursor-pointer bg-blue-100 rounded-lg">
+                  Git & GitHub
+                </div>
+              </div>
+            </Card>
+          </div>
         </div>
 
+        {/* Right Side */}
         <div className="hidden md:flex md:w-[28%]">
           <div className="sticky top-19">
             <Advertisement />
