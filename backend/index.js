9fcb16ab1aed4a5c807e246e733c8b0a18d8f41f
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-08-12 19:10:50 +0300

    Feat: Added cookie-parser and dynamic port

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
+require("dotenv").config();
+require("./connection");
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 
-require("./connection");
+const PORT = process.env.PORT || 1478;
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("You belong with me... in the system.");
 });
 
 app.listen(1478, () => {
-  console.log("🎤 Server's in its Lover era on port 1478 💖");
+  console.log(`🎤 Server's in its Lover era on port ${PORT} 💖`);
 });
