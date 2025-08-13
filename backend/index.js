c179b67cc7512232cdbb8bf76bdcee225dd40f35
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-08-13 15:47:54 +0300

    Feat: Added user authentication routes

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,13 @@ require("./connection");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
+const UserRoutes = require("./routes/user");
 
 const PORT = process.env.PORT || 1478;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/api/auth", UserRoutes);
 
 app.get("/", (req, res) => {
   res.send("You belong with me... in the system.");
