7729693857c0829f9058425d0f3f3aa2b89041ab
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-08-15 20:10:55 +0300

    Feat: Added user profile and logout routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,9 +6,13 @@ const Authentication = require("../authentication/auth");
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.post("/google", UserController.loginTroughGmail);
+router.post("/logout", Authentication.auth, UserController.logout);
 
 router.get("/self", Authentication.auth, (req, res) => {
   return res.status(200).json({ user: req.user });
 });
+router.get("/user/:id", UserController.getProfileById);
+
+router.put("/update", Authentication.auth, UserController.updateUser);
 
 module.exports = router;
