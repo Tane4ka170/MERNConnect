71b0c0210e57463f40176b92e4e92f2293bb86e3
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-08-18 16:51:15 +0300

    Feat: Added post like/dislike and fetch routes

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -4,5 +4,12 @@ const Authentication = require("../authentication/auth");
 const PostController = require("../controller/post");
 
 router.post("/", Authentication.auth, PostController.addPost);
+router.post(
+  "/likeDislike",
+  Authentication.auth,
+  PostController.likeDislikePost
+);
+
+router.get("/getAllPosts", PostController.getAllPosts);
 
 module.exports = router;
