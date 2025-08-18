7f196abe101855320e5826a11d6a3e8501161fa3
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-08-18 16:50:32 +0300

    Feat: Added post like/dislike controller

--- backend/controller/post.js
+++ backend/controller/post.js
@@ -20,3 +20,41 @@ exports.addPost = async (req, res) => {
       .json({ error: "Internal server error", message: error.message });
   }
 };
+
+exports.likeDislikePost = async (req, res) => {
+  try {
+    let selfId = req.user._id;
+    let { postId } = req.body;
+    let post = await PostModel.findById(postId);
+    if (!post) {
+      return res.status(400).json({ error: "Post not available" });
+    }
+    const index = post.likes.findIndex((id) => id.equals(selfId));
+    if (index !== -1) {
+      post.likes.splice(index, 1);
+    } else {
+      post.likes.push(selfId);
+    }
+
+    await post.save();
+    res.status(200).json({
+      message: index !== 1 ? "Post disliked" : "Post liked",
+      likes: post.likes,
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Internal server error", message: error.message });
+  }
+};
+
+exports.getAllPosts = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Internal server error", message: error.message });
+  }
+};
