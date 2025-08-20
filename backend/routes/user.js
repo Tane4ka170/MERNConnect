b04e5d29206155045084ebf50aff1b8f80a87f1f
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-08-20 17:29:25 +0300

    Feat: Added user search and friend request routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,11 +7,17 @@ router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.post("/google", UserController.loginTroughGmail);
 router.post("/logout", Authentication.auth, UserController.logout);
+router.post(
+  "/sendFriendReq",
+  Authentication.auth,
+  UserController.sendFriendRequest
+);
 
 router.get("/self", Authentication.auth, (req, res) => {
   return res.status(200).json({ user: req.user });
 });
 router.get("/user/:id", UserController.getProfileById);
+router.get("/findUser", Authentication.auth, UserController.findUser);
 
 router.put("/update", Authentication.auth, UserController.updateUser);
 
