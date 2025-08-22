e41bf3009a378a71c93a4ce5500b6a11c140d043
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-08-22 15:02:17 +0300

    Feat: Added conversation and message controllers

--- backend/controller/conversation.js
+++ backend/controller/conversation.js
@@ -9,7 +9,46 @@ exports.addConversation = async (req, res) => {
       members: { $all: [senderId, receiverId] },
     });
     if (!isConvExist) {
+      let newConversation = new ConversationModel({
+        members: [senderId, receiverId],
+      });
+      await newConversation.save();
+      let addMessage = new MessageModel({
+        sender: req.user._id,
+        conversation: newConversation._id,
+        message,
+      });
+      await addMessage.save();
+    } else {
+      let addMessage = new MessageModel({
+        sender: req.user._id,
+        conversation: newConversation._id,
+        message,
+      });
+      await addMessage.save();
     }
+
+    return res.status(201).json({ message: "Message delivered" });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Internal server error", message: error.message });
+  }
+};
+
+exports.getConversation = async (req, res) => {
+  try {
+    let loggedId = req.user._id;
+    let conversation = await ConversationModel.find({
+      members: $in[loggedId],
+    })
+      .populate("members", "-password")
+      .sort({ createdAt: -1 });
+
+    return res
+      .status(200)
+      .json({ message: "Retrieved successfully", conversation: conversation });
   } catch (error) {
     console.error(error);
     res
