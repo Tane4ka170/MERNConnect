704872a4673c1e3543f9338ef71c0106a599ce9a
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-08-23 17:08:14 +0300

    Feat: Implemented user login functionality

--- frontend/src/pages/Login/login.jsx
+++ frontend/src/pages/Login/login.jsx
@@ -1,8 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import GoogleLoginComp from "../../components/GoogleLogin/googleLoginComp";
+import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
 
-const Login = () => {
+const Login = (props) => {
+  const [loginField, setLoginField] = useState({ email: "", password: "" });
+  const navigate = useNavigate();
+
+  const onChangeInput = (event, key) => {
+    setLoginField({ ...loginField, [key]: event.target.value });
+  };
+
+  const handleLogin = async () => {
+    if (
+      loginField.email.trim().length === 0 ||
+      loginField.password.trim().length === 0
+    ) {
+      return toast.error("Kindly complete all required fields");
+    }
+    await axios
+      .post("http://localhost:1478/api/auth/login", loginField, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        props.changeLoginValue(true);
+        localStorage.setItem("isLogin", "true");
+        localStorage.setItem("userInfo", JSON.stringify(res.data.user));
+        navigate("/feeds");
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error(err?.response?.data?.error);
+      });
+  };
   return (
     <div className="w-full flex flex-col items-center justify-center">
       <div className="w-[85%] md:w-[28%] shadow-xl rounded-sm box p-10">
@@ -24,6 +55,8 @@ const Login = () => {
               type="text"
               className="w-full text-xl border-2 rounded-lg px-5 py-1"
               placeholder="Enter your email"
+              value={loginField.email}
+              onChange={(event) => onChangeInput(event, "email")}
             />
           </div>
           <div>
@@ -32,10 +65,15 @@ const Login = () => {
               type="password"
               className="w-full text-xl border-2 rounded-lg px-5 py-1"
               placeholder="Enter your password"
+              value={loginField.password}
+              onChange={(event) => onChangeInput(event, "password")}
             />
           </div>
 
-          <div className="w-full hover:bg-blue-900 bg-blue-100 text-white py-3 px-4 rounded-xl text-center cursor-pointer my-2">
+          <div
+            className="w-full hover:bg-blue-900 bg-blue-100 text-white py-3 px-4 rounded-xl text-center cursor-pointer my-2"
+            onClick={handleLogin}
+          >
             Login
           </div>
         </div>
@@ -47,6 +85,7 @@ const Login = () => {
           </Link>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
