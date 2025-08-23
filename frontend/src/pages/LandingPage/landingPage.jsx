de78376bd58b26cdf0f3fa40abb8bb404bf04515
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-08-23 17:56:43 +0300

    Feat: Passed login state prop to Google login comp

--- frontend/src/pages/LandingPage/landingPage.jsx
+++ frontend/src/pages/LandingPage/landingPage.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import GoogleLoginComp from "../../components/GoogleLogin/googleLoginComp";
 
-const LandingPage = () => {
+const LandingPage = (props) => {
   return (
     <div className="my-4 py-[50px] md:pl-[120px] px-5 md:flex justify-between">
       <div className="md:w-[40%]">
         <div className="text-4xl mx-auto text-gray-700">
           Step Into Your Professional Network
           <div className="my-3 flex mx-auto mt-[20px] bg-white gap-2 rounded-3xl w-[70%] text-black cursor-pointer">
-            <GoogleLoginComp />
+            <GoogleLoginComp changeLoginValue={props.changeLoginValue} />
           </div>
           <Link
             to={"/login"}
