7824a9f410845d87a55712904f2c80badb870125
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-08-23 17:06:55 +0300

    Feat: Added user login state and route protection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,73 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import Login from "./pages/Login/login";
+import Feeds from "./pages/Feeds/feeds";
+import SignUp from "./pages/SignUp/signUp";
+import Resume from "./pages/Resume/resume";
+import Profile from "./pages/Profile/profile";
 import Footer from "./components/Footer/footer";
+import Messages from "./pages/Messages/messages";
+import Navbar2 from "./components/Navbar2/navbar2";
+import MyNetwork from "./pages/MyNetwork/myNetwork";
 import NavbarV1 from "./components/NavbarV1/navbarV1";
 import LandingPage from "./pages/LandingPage/landingPage";
-import { Routes, Route } from "react-router-dom";
-import SignUp from "./pages/SignUp/signUp";
-import Login from "./pages/Login/login";
-import Navbar2 from "./components/Navbar2/navbar2";
-import Feeds from "./pages/Feeds/feeds";
-import MyNetwork from "./pages/MyNetwork/myNetwork";
-import Resume from "./pages/Resume/resume";
-import Messages from "./pages/Messages/messages";
-import Profile from "./pages/Profile/profile";
+import Notification from "./pages/Notification/notification";
 import AllActivities from "./pages/AllActivities/allActivities";
 import SingleActivity from "./pages/SingleActivity/singleActivity";
-import Notification from "./pages/Notification/notification";
+
+import "./App.css";
 
 function App() {
-  const isLogin = true;
+  const [isLogin, setIsLogin] = useState(localStorage.getItem("isLogin"));
+
+  const changeLoginValue = (val) => {
+    setIsLogin(val);
+  };
+
+  // const fetchData = async () => {
+  //   await axios
+  //     .post("http://localhost:1478/api/auth/login", {
+  //       email: "",
+  //       password: "",
+  //     })
+  //     .then((res) => {
+  //       console.log(res);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
   return (
     <>
       <div className="bg-gray-50 w-[100%] h-[100%] box-border">
         {isLogin ? <Navbar2 /> : <NavbarV1 />}
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signUp" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={isLogin ? <Navigate to={"/feeds"} /> : <LandingPage />}
+          />
+          <Route
+            path="/signUp"
+            element={isLogin ? <Navigate to={"/feeds"} /> : <SignUp />}
+          />
+          <Route
+            path="/login"
+            element={
+              isLogin ? (
+                <Navigate to={"/feeds"} />
+              ) : (
+                <Login changeLoginValue={changeLoginValue} />
+              )
+            }
+          />
           <Route path="/feeds" element={<Feeds />} />
           <Route path="/myNetwork" element={<MyNetwork />} />
           <Route path="/resume" element={<Resume />} />
