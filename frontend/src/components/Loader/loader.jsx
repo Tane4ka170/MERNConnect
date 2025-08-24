ae88feab6bb554f610a3bb1876b65630a77c638b
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-08-24 14:31:34 +0300

    Fix: Corrected class names for loader component

--- frontend/src/components/Loader/loader.jsx
+++ frontend/src/components/Loader/loader.jsx
@@ -3,11 +3,11 @@ import "./loader.css";
 const Loader = () => {
   return (
     <div className="fixed top-0 left-0 w-full z-100 h-full bg-gray-600/85 flex justify-center items-center">
-      <div class="loading-wave">
-        <div class="loading-bar"></div>
-        <div class="loading-bar"></div>
-        <div class="loading-bar"></div>
-        <div class="loading-bar"></div>
+      <div className="loading-wave">
+        <div className="loading-bar"></div>
+        <div className="loading-bar"></div>
+        <div className="loading-bar"></div>
+        <div className="loading-bar"></div>
       </div>
     </div>
   );
