10e774bce3c25d3ff05ffcf78acef0445d0269b7
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-08-24 15:50:26 +0300

    Feat: Rendered post data dynamically

--- frontend/src/components/Post/post.jsx
+++ frontend/src/components/Post/post.jsx
@@ -9,58 +9,56 @@ const Post = ({ profile, item, key, personalData }) => {
   const [seeMore, setSeeMore] = useState(false);
   const [comment, setComment] = useState(false);
 
-  const desc = `Over the last few months, our team at Issac and Sons has been working hard to modernize our internal development workflows. We recently rolled out a fully containerized CI/CD pipeline using GitHub Actions and Docker, and the impact on our deployment time has been incredible — 60% faster on average. We’re now focusing on performance monitoring and integrating more automated testing across all services. Huge thanks to the dev and infra teams for pushing these initiatives forward. This is how real change happens — one step at a time. #Engineering #Leadership #DevOps #ContinuousDelivery`;
+  const desc = item?.desc;
 
   const handleSendComment = (e) => {
     e.preventDefault();
   };
+
   return (
     <Card padding={0}>
       <div className="flex gap-3 p-4">
         <div className="w-12 h-12 rounded-4xl">
           <img
-            src={
-              "https://static.vecteezy.com/system/resources/previews/019/879/186/non_2x/user-icon-on-transparent-background-free-png.png"
-            }
+            src={item?.user?.profile_pic}
             alt="User"
             className="rounded-4xl w-12 h-12 border-2 border-white cursor-pointer"
           />
         </div>
 
         <div>
-          <div className="text-lg font-semibold">Jarwadi Siregar</div>
-          <div className="text-xs text-gray-950">Engineering Manager</div>
+          <div className="text-lg font-semibold">{item?.user?.f_name}</div>
+          <div className="text-xs text-gray-950">{item?.user?.headline}</div>
         </div>
       </div>
 
       <div className="text-md p-4 my-3 whitespace-pre-line flex-grow">
-        {seeMore ? desc : `${desc.slice(0, 100)}...`}{" "}
-        <span
-          className="cursor-pointer text-gray-700"
-          onClick={() => setSeeMore((prev) => !prev)}
-        >
-          {seeMore ? "Show Less" : "Show More"}
-        </span>
+        {seeMore ? desc : `${desc.slice(0, 100)}...`}
+        {desc.length < 100 ? null : (
+          <span
+            className="cursor-pointer text-gray-700"
+            onClick={() => setSeeMore((prev) => !prev)}
+          >
+            {seeMore ? "Show Less" : "Show More"}
+          </span>
+        )}
       </div>
 
       <div className="w-[100%] h-[300px]">
-        <img
-          src={
-            "https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg?cs=srgb&dl=pexels-bri-schneiter-28802-346529.jpg&fm=jpg"
-          }
-          alt=""
-          className="w-full h-full"
-        />
+        <img src={item?.imageLink} alt="" className="w-full h-full" />
       </div>
 
       <div className="my-2 p-4 flex justify-between items-center">
         <div className="flex gap-1 items-center">
           <ThumbUpIcon sx={{ color: "blue", fontSize: 20 }} />{" "}
-          <div className="text-sm text-gray-900"> 92 Likes</div>
+          <div className="text-sm text-gray-900"> {item?.likes} Likes</div>
         </div>
         <div className="flex gap-1 items-center">
           <ThumbUpIcon sx={{ color: "blue", fontSize: 20 }} />{" "}
-          <div className="text-sm text-gray-900"> 6 Comments</div>
+          <div className="text-sm text-gray-900">
+            {" "}
+            {item?.comments} Comments
+          </div>
         </div>
       </div>
 
