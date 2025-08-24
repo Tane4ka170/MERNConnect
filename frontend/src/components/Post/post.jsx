c955d50838325530cebab9014f555d2567e7838c
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-08-24 15:10:21 +0300

    Feat: Added props for post and user data

--- frontend/src/components/Post/post.jsx
+++ frontend/src/components/Post/post.jsx
@@ -5,7 +5,7 @@ import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
 import CommentIcon from "@mui/icons-material/Comment";
 import SendIcon from "@mui/icons-material/Send";
 
-const Post = ({ profile }) => {
+const Post = ({ profile, item, key, personalData }) => {
   const [seeMore, setSeeMore] = useState(false);
   const [comment, setComment] = useState(false);
 
