f037f86ed7fa3d354ad5e9057d98d4f81e650dea
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-08-30 16:38:52 +0300

    feat: implemented dynamic img upload

--- frontend/src/components/ImageModal/imageModal.jsx
+++ frontend/src/components/ImageModal/imageModal.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
-const ImageModal = ({ isCircular }) => {
+const ImageModal = ({ isCircular, selfData }) => {
+  const [imgLink, setImgLink] = useState(
+    isCircular ? selfData?.profile_pic : selfData?.cover_pic
+  );
+  const [loading, setLoading] = useState(false);
+
+  const handleInputImage = async (e) => {
+    const files = e.target.files[0];
+    const data = new FormData();
+    data.append("file", files);
+
+    data.append("upload_preset", "NetWorkin");
+    setLoading(true);
+
+    try {
+      const response = await axios.post(
+        "https://api.cloudinary.com/v1_1/doo1ifq0i/image/upload",
+        data
+      );
+      const imageUrl = response.data.secure_url;
+      setImgLink(imageUrl);
+    } catch (error) {
+      console.error("Upload error:", error.response?.data || error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className="p-5 relative flex items-center flex-col h-full">
-      {/* {isCircular ? "Round Image" : "Full-Width Image"} */}
       {isCircular ? (
         <img
-          src={
-            "https://cdn.pixabay.com/photo/2016/08/21/18/48/emoticon-1610518_1280.png"
-          }
+          src={imgLink}
           alt=""
           className="w-[150px] h-[150px] rounded-full"
         />
       ) : (
         <img
-          src={"https://cdn.mos.cms.futurecdn.net/eQjVBwXHWHAyWThasKjHRb.png"}
+          src={imgLink}
           alt=""
           className="rounded-xl w-full h-[200px] object-cover"
         />
@@ -25,11 +51,19 @@ const ImageModal = ({ isCircular }) => {
       >
         Add File
       </label>
-      <input type="file" id="btn-submit" className="hidden" />
+      <input
+        type="file"
+        id="btn-submit"
+        className="hidden"
+        onChange={handleInputImage}
+      />
 
       <div className="absolute bottom-10 right-0 p-3 bg-blue-200 rounded-2xl cursor-pointer">
         Send
       </div>
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
     </div>
   );
 };
