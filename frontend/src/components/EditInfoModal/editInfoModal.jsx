c65e936f71715338342188b7723250ef7c158faa
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-08-31 17:24:38 +0300

    feat: implemented Edit Info Modal functionality

--- frontend/src/components/EditInfoModal/editInfoModal.jsx
+++ frontend/src/components/EditInfoModal/editInfoModal.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-const EditInfoModal = () => {
+const EditInfoModal = ({ handleEditFunction, selfData }) => {
+  const [data, setData] = useState({
+    f_name: selfData.f_name,
+    headline: selfData.headline,
+    curr_company: selfData.curr_company,
+    curr_location: selfData.curr_location,
+  });
+
+  const onChangeHandle = (event, key) => {
+    setData({ ...data, [key]: event.target.value });
+  };
+
+  const handleSaveBtn = async () => {
+    let newData = { ...selfData, ...data };
+    handleEditFunction(newData);
+  };
   return (
     <div className="mt-8 w-full h-[350px] overflow-auto">
       <div className="w-full mb-4">
@@ -10,6 +25,8 @@ const EditInfoModal = () => {
           type="text"
           className="p-2 mt-1 w-full border-1 rounded-md"
           placeholder="Type your full name"
+          value={data.f_name}
+          onChange={(e) => onChangeHandle(e, "f_name")}
         />
       </div>
 
@@ -20,6 +37,8 @@ const EditInfoModal = () => {
           cols={10}
           rows={3}
           className="p-2 mt-1 w-full border-1 rounded-md"
+          value={data.headline}
+          onChange={(e) => onChangeHandle(e, "headline")}
         ></textarea>
       </div>
 
@@ -30,6 +49,8 @@ const EditInfoModal = () => {
           type="text"
           className="p-2 mt-1 w-full border-1 rounded-md"
           placeholder="Type your current company name"
+          value={data.curr_company}
+          onChange={(e) => onChangeHandle(e, "curr_company")}
         />
       </div>
 
@@ -40,9 +61,14 @@ const EditInfoModal = () => {
           type="text"
           className="p-2 mt-1 w-full border-1 rounded-md"
           placeholder="Type your current location"
+          value={data.curr_location}
+          onChange={(e) => onChangeHandle(e, "curr_location")}
         />
       </div>
-      <div className="bg-blue-900 text-white w-fit py-1 px-3 cursor-pointer rounded-2xl">
+      <div
+        className="bg-blue-900 text-white w-fit py-1 px-3 cursor-pointer rounded-2xl"
+        onClick={handleSaveBtn}
+      >
         Save
       </div>
     </div>
