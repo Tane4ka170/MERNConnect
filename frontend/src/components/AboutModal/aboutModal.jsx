73ce3d3a34f39801da1e9289ca46af0eb98a29f9
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-09-01 17:30:37 +0300

    feat: Added resume upload functionality

--- frontend/src/components/AboutModal/aboutModal.jsx
+++ frontend/src/components/AboutModal/aboutModal.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 
 const AboutModal = ({ handleEditFunction, selfData }) => {
@@ -6,9 +7,47 @@ const AboutModal = ({ handleEditFunction, selfData }) => {
     skillInp: selfData?.skills?.join(","),
     resume: selfData?.resume,
   });
+  const [loading, setLoading] = useState(false);
 
   const onChangeHandle = (event, key) => {
     setData({ ...data, [key]: event.target.value });
+  };
+
+  const handleInputImage = async (e) => {
+    const files = e.target.files[0];
+    const data = new FormData();
+    data.append("file", files);
+
+    data.append("upload_preset", "NetWorkin");
+    setLoading(true);
+
+    try {
+      const response = await axios.post(
+        "https://api.cloudinary.com/v1_1/doo1ifq0i/image/upload",
+        data
+      );
+      const imageUrl = response.data.secure_url;
+      setData({ ...data, resume: imageUrl });
+    } catch (error) {
+      console.error("Upload error:", error.response?.data || error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleOnSave = async () => {
+    let arr = (data?.skillInp || "")
+      .split(",")
+      .map((s) => s.trim())
+      .filter((s) => s);
+
+    let newData = {
+      ...selfData,
+      about: data?.about,
+      skills: arr,
+      resume: data?.resume,
+    };
+    handleEditFunction(newData);
   };
   return (
     <div className="my-8">
@@ -43,9 +82,18 @@ const AboutModal = ({ handleEditFunction, selfData }) => {
         >
           Upload Your Resume
         </label>
-        <input type="file" className="hidden" id="resumeUpload" />
+        <input
+          type="file"
+          className="hidden"
+          id="resumeUpload"
+          onChange={handleInputImage}
+        />
+        {data.resume && <div className="my-2">{data.resume}</div>}
       </div>
-      <div className="bg-blue-900 text-white w-fit py-1 px-3 cursor-pointer rounded-2xl">
+      <div
+        className="bg-blue-900 text-white w-fit py-1 px-3 cursor-pointer rounded-2xl"
+        onClick={handleOnSave}
+      >
         Save
       </div>
     </div>
