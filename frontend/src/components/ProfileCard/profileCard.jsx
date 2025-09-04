05672d35bddbee053a049b03957a638de1f55677
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-09-04 16:04:16 +0300

    feat: Profile cards now link to profiles

--- frontend/src/components/ProfileCard/profileCard.jsx
+++ frontend/src/components/ProfileCard/profileCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Card from "../Card/card";
+import { Link } from "react-router-dom";
 
 const ProfileCard = (props) => {
   return (
     <Card padding={1}>
-      <div className="relative h-25">
+      <Link to={`/profile/${props?.data?._id}`} className="relative h-25">
         <div className="relative h-22 w-full rounded-md">
           <img src={props.data?.cover_pic} alt="" />
         </div>
@@ -16,7 +17,7 @@ const ProfileCard = (props) => {
             className="rounded-4xl border-2 h-16 w-16 border-white cursor-pointer"
           />
         </div>
-      </div>
+      </Link>
 
       <div className="p-5">
         <div className="text-xl">{props.data?.f_name}</div>
