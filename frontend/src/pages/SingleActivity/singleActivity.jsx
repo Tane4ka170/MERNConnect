fa080ea0af0785ae0e387a2a68989849eee61efa
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-09-05 14:30:38 +0300

    feat: implemented dynamic single activity page

--- frontend/src/pages/SingleActivity/singleActivity.jsx
+++ frontend/src/pages/SingleActivity/singleActivity.jsx
@@ -1,15 +1,41 @@
-import Advertisement from "../../components/Advertisement/advertisement";
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
 import Card from "../../components/Card/card";
 import Post from "../../components/Post/post";
 import ProfileCard from "../../components/ProfileCard/profileCard";
+import Advertisement from "../../components/Advertisement/advertisement";
 
 const SingleActivity = () => {
+  const { id, postId } = useParams;
+  const [ownData, setOwnData] = useState(null);
+  const [post, setPost] = useState(null);
+
+  const fetchDataOnLoad = async () => {
+    await axios
+      .get(`http://localhost:1478/api/post/getPostById/${postId}`)
+      .then((res) => {
+        setPost(res.data.post);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(err?.response?.data?.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchDataOnLoad();
+    let userData = localStorage.getItem("userInfo");
+    setOwnData(userData ? JSON.parse(userData) : null);
+  }, []);
   return (
     <div className="px-5 xl:px-50 py-9 flex gap-5 w-full mt-5 bg-gray-300">
       {/* Left */}
       <div className="w-[21%] sm:block sm:w-[23%] hidden py-5">
         <div className="h-fit">
-          <ProfileCard />
+          <ProfileCard data={post?.user} />
         </div>
       </div>
 
@@ -18,7 +44,7 @@ const SingleActivity = () => {
         <div>
           <Card padding={1}>
             <div>
-              <Post />
+              <Post personalData={ownData} item={post} />
             </div>
           </Card>
         </div>
