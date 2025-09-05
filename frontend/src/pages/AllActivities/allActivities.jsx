fde6d09fb0a079f6fd8b6611ad1216664cefc54f
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-09-05 14:30:18 +0300

    feat: Implemented dynamic activities page

--- frontend/src/pages/AllActivities/allActivities.jsx
+++ frontend/src/pages/AllActivities/allActivities.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import { useEffect, useState } from "react";
+
+import axios from "axios";
 import { useParams } from "react-router-dom";
+
 import Card from "../../components/Card/card";
+import Post from "../../components/Post/post";
 import ProfileCard from "../../components/ProfileCard/profileCard";
 import Advertisement from "../../components/Advertisement/advertisement";
-import Post from "../../components/Post/post";
 
 const AllActivities = () => {
   const { id } = useParams();
+  const [post, setPost] = useState([]);
+  const [ownData, setOwnData] = useState(null);
+
+  const fetchDataOnLoad = async () => {
+    await axios
+      .get(`http://localhost:1478/api/post/getAllPostByUser/${id}`)
+      .then((res) => {
+        setPost(res?.data?.posts);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(err?.response?.data?.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchDataOnLoad();
+
+    let userData = localStorage.getItem("userInfo");
+    setOwnData(userData ? JSON.parse(userData) : null);
+  }, [id]);
+
   return (
     <div className="px-5 xl:px-50 py-9 flex gap-5 w-full mt-5 bg-gray-300">
       {/* Left */}
       <div className="w-[21%] sm:block sm:w-[23%] hidden py-5">
         <div className="h-fit">
-          <ProfileCard />
+          <ProfileCard data={post[0]?.user} />
         </div>
       </div>
 
@@ -25,9 +50,13 @@ const AllActivities = () => {
               Posts
             </div>
             <div className="my-2 flex flex-col gap-2">
-              <div>
-                <Post />
-              </div>
+              {post?.map((item, index) => {
+                return (
+                  <div key={index}>
+                    <Post item={item} personalData={ownData} />
+                  </div>
+                );
+              })}
             </div>
           </Card>
         </div>
