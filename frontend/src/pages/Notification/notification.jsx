4e9d432a29690e6681f0c883d766dcde4c3f071a
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-09-07 17:51:01 +0300

    feat: Added dynamic notification page

--- frontend/src/pages/Notification/notification.jsx
+++ frontend/src/pages/Notification/notification.jsx
@@ -1,15 +1,65 @@
-import React from "react";
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+import Card from "../../components/Card/card";
 import ProfileCard from "../../components/ProfileCard/profileCard";
 import Advertisement from "../../components/Advertisement/advertisement";
-import Card from "../../components/Card/card";
 
 const Notification = () => {
+  const [ownData, setOwnData] = useState();
+  const [notifications, setNotifications] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let userData = localStorage.getItem("userInfo");
+    setOwnData(userData ? JSON.parse(userData) : null);
+
+    fetchNotificationData();
+  }, []);
+
+  const fetchNotificationData = async () => {
+    await axios
+      .get("http://localhost:1478/api/notification", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setNotifications(res.data.notifications);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("An error occurred");
+      });
+  };
+
+  const handleOnClickNotification = async (item) => {
+    await axios
+      .put(
+        "http://localhost:1478/api/notification/isRead",
+        {
+          notificationId: item._id,
+        },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        if (item.type === "comment") {
+          navigate(`/profile/${ownData?.id}/activities/${item?.postId}`);
+        } else {
+          navigate("/myNetwork");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("An error occurred");
+      });
+  };
   return (
     <div className="px-5 xl:px-50 py-9 flex gap-5 w-full mt-5 bg-gray-300">
       {/* Left */}
       <div className="w-[21%] sm:block sm:w-[23%] hidden py-5">
         <div className="h-fit">
-          <ProfileCard />
+          <ProfileCard data={ownData} />
         </div>
       </div>
 
@@ -19,30 +69,23 @@ const Notification = () => {
           <Card padding={0}>
             <div className="w-full">
               {/* For each notification */}
-              <div
-                className={`border-b-1 cursor-pointer flex gap-4 items-center border-gray-700 p-3`}
-              >
-                <img
-                  src={
-                    "https://cdn.pixabay.com/photo/2017/03/21/13/27/evil-2162179_1280.png"
-                  }
-                  alt=""
-                  className="rounded-4xl cursor-pointer w-15 h-15"
-                />
-                <div>Dummy User has invited you to connect</div>
-              </div>
-              <div
-                className={`border-b-1 cursor-pointer flex gap-4 items-center border-gray-700 p-3`}
-              >
-                <img
-                  src={
-                    "https://cdn.pixabay.com/photo/2017/03/21/13/27/evil-2162179_1280.png"
-                  }
-                  alt=""
-                  className="rounded-4xl cursor-pointer w-15 h-15"
-                />
-                <div>Dummy User left a comment on your post</div>
-              </div>
+              {notifications.map((item, index) => {
+                return (
+                  <div
+                    className={`border-b-1 cursor-pointer flex gap-4 items-center border-gray-700 p-3 ${
+                      item?.isRead ? "bg-gray-300" : "bg-blue-50"
+                    }`}
+                    onClick={handleOnClickNotification(item)}
+                  >
+                    <img
+                      src={item?.sender?.profile_pic}
+                      alt=""
+                      className="rounded-4xl cursor-pointer w-15 h-15"
+                    />
+                    <div>{item?.content}</div>
+                  </div>
+                );
+              })}
             </div>
           </Card>
         </div>
