d5f883f969dcddd14b20c9cd33ab2c351affb99b
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-09-11 14:21:32 +0300

    feat: added messaging functionality

--- frontend/src/pages/Messages/messages.jsx
+++ frontend/src/pages/Messages/messages.jsx
@@ -15,7 +15,10 @@ const Messages = () => {
   const [ownData, setOwnData] = useState(null);
   const [activeConvId, setActiveConvId] = useState(null);
   const [selectedConvDetails, setSelectedConvDetails] = useState(null);
-  const [messages,setMessages] = useState([])
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [imgLink, setImgLink] = useState(null);
+  const [messageText, setMessageText] = useState("");
 
   useEffect(() => {
     let userData = localStorage.getItem("userInfo");
@@ -60,13 +63,54 @@ const Messages = () => {
         withCredentials: true,
       })
       .then((res) => {
-        console.log(res);
+        setMessages(res?.data?.message);
       })
       .catch((err) => {
         console.log(err);
         alert("An error occurred");
       });
   };
+
+  const handleInputImage = async (e) => {
+    const files = e.target.files[0];
+    const data = new FormData();
+    data.append("file", files);
+
+    data.append("upload_preset", "NetWorkin");
+    setLoading(true);
+
+    try {
+      const response = await axios.post(
+        "https://api.cloudinary.com/v1_1/doo1ifq0i/image/upload",
+        data
+      );
+      const imageUrl = response.data.secure_url;
+      setImgLink(imageUrl);
+    } catch (error) {
+      console.error("Upload error:", error.response?.data || error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSendMessage = async () => {
+    await axios
+      .post(
+        `http://localhost:1478/api/message`,
+        {
+          conversation: activeConvId,
+          message: messageText,
+          picture: imgLink,
+        },
+        { withCredentials: true }
+      )
+      .then((res) => {})
+      .catch((err) => {
+        console.log(err);
+        alert("An error occurred");
+      });
+  };
+
   return (
     <div className="px-5 xl:px-50 py-9 flex gap-5 w-full mt-5 bg-gray-300">
       <div className="w-full justify-between flex pt-5">
@@ -97,18 +141,17 @@ const Messages = () => {
                       activeConvId={activeConvId}
                     />
                   );
-                }
-                    />
-                  );
                 })}
               </div>
 
               <div className="w-full md:w-[60%] border-gray-400">
                 <div className="border-gray-950 py-2 px-4 border-b-2 flex justify-between items-center">
                   <div>
-                    <p className="text-sm font-semibold">User 1</p>
+                    <p className="text-sm font-semibold">
+                      {selectedConvDetails?.f_name}
+                    </p>
                     <p className="text-sm text-gray-950">
-                      Baby, just say yes ✅
+                      {selectedConvDetails?.headline}
                     </p>
                   </div>
                   <div>
@@ -119,49 +162,56 @@ const Messages = () => {
                 <div className="h-[360px] w-full overflow-auto border-b-1 border-gray-950">
                   <div className="w-full border-b-1 border-gray-950 gap-3 p-4">
                     <img
-                      src={
-                        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/User_icon-cp.png/1200px-User_icon-cp.png"
-                      }
+                      src={selectedConvDetails?.profile_pic}
                       alt=""
                       className="rounded-[100%] cursor-pointer w-16 h-15"
                     />
                     <div className="my-2">
-                      <p className="text-sm font-semibold">User 1</p>
+                      <p className="text-sm font-semibold">
+                        {selectedConvDetails?.f_name}
+                      </p>
                       <p className="text-sm text-gray-950">
-                        Baby, just say yes ✅
+                        {selectedConvDetails?.headline}
                       </p>
                     </div>
                   </div>
 
                   <div className="w-full">
                     {/* for each messages */}
-                    <div className="flex w-full cursor-pointer border-gray-800 gap-3 p-4">
-                      <div className="shrink-0">
-                        <img
-                          src={
-                            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/User_icon-cp.png/1200px-User_icon-cp.png"
-                          }
-                          alt=""
-                          className="w-8 h-8 rounded-[100%] cursor-pointer"
-                        />
-                      </div>
+                    {messages?.map((item, index) => {
+                      return (
+                        <div
+                          className="flex w-full cursor-pointer border-gray-800 gap-3 p-4"
+                          key={index}
+                        >
+                          <div className="shrink-0">
+                            <img
+                              src={item?.sender?.profile_pic}
+                              alt=""
+                              className="w-8 h-8 rounded-[100%] cursor-pointer"
+                            />
+                          </div>
 
-                      <div className="mb-2 w-full">
-                        <div className="text-md">User 1</div>
-                        <div className="text-sm mt-6 hover:bg-gray-800 hover:text-white">
-                          Shaking it off 🫧
+                          <div className="mb-2 w-full">
+                            <div className="text-md">
+                              {item?.sender?.f_name}
+                            </div>
+                            <div className="text-sm mt-6 hover:bg-gray-800 hover:text-white">
+                              {item?.message}
+                            </div>
+                            {item?.picture && (
+                              <div className="my-2">
+                                <img
+                                  src={item?.picture}
+                                  alt=""
+                                  className="w-[240px] h-[180px] rounded-md"
+                                />
+                              </div>
+                            )}
+                          </div>
                         </div>
-                        <div className="my-2">
-                          <img
-                            src={
-                              "https://sb.kaleidousercontent.com/67418/1920x1100/ffa7b88ebb/transparent.png"
-                            }
-                            alt=""
-                            className="w-[240px] h-[180px] rounded-md"
-                          />
-                        </div>
-                      </div>
-                    </div>
+                      );
+                    })}
                   </div>
                 </div>
 
@@ -171,6 +221,8 @@ const Messages = () => {
                     rows={4}
                     className="bg-gray-200 outline-0 rounded-xl text-sm w-full p-3"
                     placeholder="Type a message"
+                    value={messageText}
+                    onChange={(e) => setMessageText(e.target.value)}
                   ></textarea>
                 </div>
                 <div className="p-3 flex justify-between">
@@ -178,11 +230,21 @@ const Messages = () => {
                     <label htmlFor="messageImage" className="cursor-pointer">
                       <ImageIcon />
                     </label>
-                    <input type="file" id="messageImage" className="hidden" />
+                    <input
+                      type="file"
+                      id="messageImage"
+                      className="hidden"
+                      onClick={handleInputImage}
+                    />
                   </div>
-                  <div className="px-3 py-1 cursor-pointer rounded-2xl border-1 bg-blue-50">
-                    Send
-                  </div>
+                  {!loading && (
+                    <div
+                      className="px-3 py-1 cursor-pointer rounded-2xl border-1 bg-blue-50"
+                      onClick={handleSendMessage}
+                    >
+                      Send
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
