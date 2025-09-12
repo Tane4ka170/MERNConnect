296ce445ad5a7e2c23b63ba9e5b165028f46508c
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-09-12 16:55:50 +0300

    feat: Added real-time messaging with Socket.IO

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,27 @@ const CommentRoutes = require("./routes/comment");
 const ConversationRoutes = require("./routes/conversation");
 const MessageRoutes = require("./routes/message");
 const cors = require("cors");
+const { Server } = require("socket.io");
+const http = require("http");
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] },
+});
+
+io.on("connection", (socket) => {
+  console.log("User joined");
+
+  socket.on("joinConversation", (conversationId) => {
+    console.log(`User has joined the conversation with ID ${conversationId}`);
+    socket.join(conversationId);
+  });
+
+  socket.on("sendMessage", (convId, messageDetail) => {
+    console.log("Message successfully sent");
+    io.to(convId).emit("receiveMessage", messageDetail);
+  });
+});
 
 const PORT = process.env.PORT || 1478;
 
@@ -27,6 +48,6 @@ app.get("/", (req, res) => {
   res.send("You belong with me... in the system.");
 });
 
-app.listen(1478, () => {
+server.listen(PORT, () => {
   console.log(`🎤 Server's in its Lover era on port ${PORT} 💖`);
 });
